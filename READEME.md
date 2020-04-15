fdf7094869e0fba4a91460459ef3570623513945
JunioDamasceno <62802088+JunioDamasceno@users.noreply.github.com>
2020-04-15 20:31:07 -0300

    Add files via upload

---
+++ READEME.md
@@ -0,0 +1,3 @@
+#Gerenciador de Contas e Senhas
+
+**gerenciadorcs** é um programa que gerencia contas de usuaŕios e senhas.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup( name = 'gerenciadorcs',
+       version = '1.0.0',
+       author = 'Junio da Silva Damasceno',
+       author_email = 'juniowin@yahoo.com.br',
+       packages = ['gerenciadorcs'],
+       description = 'um gerenciador de contas e senhas de usuarios',
+       url = 'https://github.com/JunioDamasceno/gerenciadorcs',
+       license = 'MIT',
+       keywords = 'gerenciador de senhas',
+       classifiers = [
+           'Development Status :: 5 - Production/Stable',
+           'Intended Audience :: Developers',
+           'Licence :: OSI Aproved :: MIT licence',
+           'Natural Language :: Portuguese (Brazilian)',
+       ]
+)
+
