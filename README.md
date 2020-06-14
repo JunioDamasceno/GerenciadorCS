1a178f4496ddbb38b7d01c86be5d3a52021f8829
JunioDamasceno <62802088+JunioDamasceno@users.noreply.github.com>
2020-06-14 13:44:12 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+#Gerenciador de Contas e Senhas
+
+**gerenciadorcs** é um gerenciador de contas, usuários e senhas.
+
+Notas da Versão 2.2:
+-A lista de contas usuários e senhas cadastradas agora é exibida em ordem alfanumérica, o ordenamento ocorre apenas pelo nome das contas.
+-Nova rebuild devido a atualizações de segurança do Ubuntu para sistemas operacionais baseados em Ubuntu.
+-Otimizações no fluxo do código.
+-Correção de ícones não exibidos na versão anterior.
+
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup( name = 'gerenciadorcs',
+       version = '2.2',
+       author = 'Junio da Silva Damasceno',
+       author_email = 'juniowin@yahoo.com.br',
+       packages = ['gerenciadorcs'],
+       data_files=[
+           ('png', ['gerenciadorcs/icone_64x64.png']),
+           ('desktop', ['gerenciadorcs/gerenciadorcs.desktop']),
+           ('glade', ['gerenciadorcs/interface.glade']),
+           ],
+       description = 'um gerenciador de contas, usuários e senhas',
+       url = 'https://github.com/JunioDamasceno/gerenciadorcs',
+       license = 'MIT',
+       keywords = ['gerenciador de senhas', 'Key manager', 'keymanager'],
+       classifiers = [
+           'Development Status :: 5 - Production/Stable',
+           'Intended Audience :: Developers',
+           'Licence :: OSI Aproved :: MIT licence',
+           'Natural Language :: Portuguese (Brazilian)',
+       ]
+)
+
