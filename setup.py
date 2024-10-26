60d37ed1ee59faef5c9d83175faca1c01e5c33e6
Junio da Silva Damasceno <62802088+JunioDamasceno@users.noreply.github.com>
2024-10-26 08:32:40 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name = 'gerenciadorcs',
-       version = '3.2',
+       version = '3.2.1',
        author = 'Junio da Silva Damasceno',
        author_email = 'juniowin@yahoo.com.br',
        packages = ['gerenciadorcs'],
